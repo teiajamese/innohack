7e4cb54037da0a182c78887c0ccc53187bd79c7a
teiajamese <teiaatkins@gmail.com>
2017-05-20 14:27:03 -0400

    new changes

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 $servername = "162.243.45.50";
 $username = "innohackr";
 $password = "innohackrpassword";
+$dbname = "bgv";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
@@ -11,5 +12,15 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 echo "Connected successfully";
+
+
+$sql = "INSERT INTO Users (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
 ?>

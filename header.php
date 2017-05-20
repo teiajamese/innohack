5ebbf6e70043109b7e321a64434e7e2ab8944be7
teiajamese <teiaatkins@gmail.com>
2017-05-20 13:51:48 -0400

    added db connect info

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
+$servername = "162.243.45.50";
+$username = "innohackr";
+$password = "innohackrpassword";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
